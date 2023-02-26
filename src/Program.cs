0b96c5217d24894a40803e2998b7004b5255bde6
Philipp Meier <philipp.meier@gmx.at>
2023-02-26 16:20:31 +0100

    fix: Removed blocking reader-call

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,3 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +10,7 @@ var connStr = new SqliteConnectionStringBuilder {
     Mode = SqliteOpenMode.ReadOnly,
     DataSource = mbtilesPath
 };
-builder.Services.AddScoped(_ => new SqliteConnection(connStr.ToString()));
+builder.Services.AddTransient(_ => new SqliteConnection(connStr.ToString()));
 
 var app = builder.Build();
 
@@ -23,28 +21,23 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+const string tileDataQuery = "SELECT tile_data FROM tiles WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y";
 app.MapGet("/{z:int}/{x:int}/{y:int}.png", async (HttpContext context, SqliteConnection db, int z, int x, int y) => {
     context.Response.Headers.Add("Cache-Control", "max-age=58362, stale-while-revalidate=604800, stale-if-error=604800");
 
     await db.OpenAsync();
-    using (var command = db.CreateCommand()) {
-        command.CommandText =
-        @"
-            SELECT tile_data
-            FROM tiles
-            WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y
-        ";
-        command.Parameters.AddWithValue("$z", z);
-        command.Parameters.AddWithValue("$x", x);
-        command.Parameters.AddWithValue("$y", y);
-        
-        using (var reader = await command.ExecuteReaderAsync()) {
-            if (await reader.ReadAsync() && reader["tile_data"] is byte[] tileData)
-                return Results.File(tileData, contentType: "image/png");
-            else 
-                return Results.NoContent();
-        }
-    }
+    await using var command = db.CreateCommand();
+
+    command.CommandText = tileDataQuery;
+    command.Parameters.AddWithValue("$z", z);
+    command.Parameters.AddWithValue("$x", x);
+    command.Parameters.AddWithValue("$y", y);
+
+    await using var reader = await command.ExecuteReaderAsync();
+    if (await reader.ReadAsync())
+        return Results.File(await reader.GetFieldValueAsync<byte[]>(0), contentType: "image/png");
+    
+    return Results.NoContent();
 });
 
 app.UseHttpsRedirection();
