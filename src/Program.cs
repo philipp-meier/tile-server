8190921bb7dc50a6c5f21b1621e4852841ba82af
Philipp Meier <philipp.meier@gmx.at>
2023-02-26 20:24:22 +0100

    feat: Minor optimizations.

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,21 @@
+using System.Data;
 using Microsoft.Data.Sqlite;
-
-var builder = WebApplication.CreateBuilder(args);
 
 var mbtilesPath = Path.Combine(Directory.GetCurrentDirectory(), "data", "map_data.mbtiles");
 if (!File.Exists(mbtilesPath))
     throw new FileNotFoundException(mbtilesPath);
 
+var builder = WebApplication.CreateBuilder(args);
+
 var connStr = new SqliteConnectionStringBuilder {
     Mode = SqliteOpenMode.ReadOnly,
     DataSource = mbtilesPath
-};
-builder.Services.AddTransient(_ => new SqliteConnection(connStr.ToString()));
+}.ToString();
+builder.Services.AddTransient(_ => new SqliteConnection(connStr));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-const string tileDataQuery = "SELECT tile_data FROM tiles WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y";
+const string tileDataQuery = "SELECT tile_data FROM tiles WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y LIMIT 1";
 app.MapGet("/{z:int}/{x:int}/{y:int}.png", async (HttpContext context, SqliteConnection db, int z, int x, int y) => {
     context.Response.Headers.Add("Cache-Control", "max-age=58362, stale-while-revalidate=604800, stale-if-error=604800");
 
@@ -33,12 +27,15 @@ app.MapGet("/{z:int}/{x:int}/{y:int}.png", async (HttpContext context, SqliteCon
     command.Parameters.AddWithValue("$x", x);
     command.Parameters.AddWithValue("$y", y);
 
-    await using var reader = await command.ExecuteReaderAsync();
+    await using var reader = await command.ExecuteReaderAsync(CommandBehavior.SequentialAccess);
     if (await reader.ReadAsync())
         return Results.File(await reader.GetFieldValueAsync<byte[]>(0), contentType: "image/png");
     
     return Results.NoContent();
 });
+
+if (!app.Environment.IsDevelopment())
+    app.UseHsts();
 
 app.UseHttpsRedirection();
 app.Run();
