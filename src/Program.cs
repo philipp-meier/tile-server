e9041136d39e89e0e33d8d40f496dc561a949dd9
Philipp Meier <philipp.meier@gmx.at>
2023-09-16 15:31:47 +0200

    feat(config)!: Unified path configuration.

--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,21 @@
 using System.Data;
 using Microsoft.Data.Sqlite;
 
-const string mbtilesFileName = "map_data.mbtiles";
-var mbtilesPath = Environment.GetEnvironmentVariable("ASPNETCORE_MBTILES_PATH") ??
-    Path.Combine("data", mbtilesFileName);
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
 
+var mbtilesPath = builder.Configuration["TileServer:MbTilesPath"];
 if (!File.Exists(mbtilesPath))
     throw new FileNotFoundException($"Required .mbtiles-file \"{mbtilesPath}\" not found.");
 
-var builder = WebApplication.CreateBuilder(args);
-
-var connStr = new SqliteConnectionStringBuilder {
-    Mode = SqliteOpenMode.ReadOnly,
-    DataSource = mbtilesPath
-}.ToString();
+var connStr = $"Data Source={mbtilesPath};Mode=ReadOnly";
 builder.Services.AddTransient(_ => new SqliteConnection(connStr));
 
 var app = builder.Build();
 
 const string tileDataQuery = "SELECT tile_data FROM tiles WHERE zoom_level = $z AND tile_column = $x AND tile_row = $y LIMIT 1";
-app.MapGet("/{z:int}/{x:int}/{y:int}.png", async (HttpContext context, SqliteConnection db, int z, int x, int y) => {
+app.MapGet("/{z:int}/{x:int}/{y:int}.png", async (HttpContext context, SqliteConnection db, int z, int x, int y) =>
+{
     context.Response.Headers.Add("Cache-Control", "max-age=58362, stale-while-revalidate=604800, stale-if-error=604800");
 
     await db.OpenAsync();
@@ -33,7 +29,7 @@ app.MapGet("/{z:int}/{x:int}/{y:int}.png", async (HttpContext context, SqliteCon
     await using var reader = await command.ExecuteReaderAsync(CommandBehavior.SequentialAccess);
     if (await reader.ReadAsync())
         return Results.File(await reader.GetFieldValueAsync<byte[]>(0), contentType: "image/png");
-    
+
     return Results.NoContent();
 });
 
